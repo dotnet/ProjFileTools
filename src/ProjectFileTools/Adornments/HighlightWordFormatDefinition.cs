4444b00b206ffab390a855b85b8101002bb69ca9
Brandon Orooji <t-broro@microsoft.com>
2017-05-18 15:54:55 -0700

    Added the highlight selected text feature.

---
+++ src/ProjectFileTools/Adornments/HighlightWordFormatDefinition.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Utilities;
+using System.ComponentModel.Composition;
+using System.Windows.Media;
+
+namespace ProjectFileTools.Adornments
+{
+    // Used by HighLightWordTag to get the acquired tags' background color.
+    [Export(typeof(EditorFormatDefinition))]
+    [Name("MarkerFormatDefinition/HighlightWordFormatDefinition")]
+    internal class HighlightWordFormatDefinition : MarkerFormatDefinition
+    {
+        public HighlightWordFormatDefinition()
+        {
+            this.BackgroundColor = Color.FromArgb(127, 170, 170, 170);
+            this.DisplayName = "Highlight Word";
+            this.ZOrder = 5;
+        }
+    }
+}
