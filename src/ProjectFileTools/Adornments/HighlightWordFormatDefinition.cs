50ba18f44b99bafacb58f77cfa63d10039f45139
Brandon Orooji <t-broro@microsoft.com>
2017-05-23 14:04:34 -0700

    Changes a lot of porperties to fields Removes unnescessary this qualifiers Sets the Backgroundcolor of the tags to the Highlight Reference Background Color (under Fonts and Colors in options)

--- src/ProjectFileTools/Adornments/HighlightWordFormatDefinition.cs
+++ src/ProjectFileTools/Adornments/HighlightWordFormatDefinition.cs
@@ -1,5 +1,9 @@
-﻿using System.ComponentModel.Composition;
+﻿using System;
+using System.ComponentModel.Composition;
 using System.Windows.Media;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
 using Microsoft.VisualStudio.Text.Classification;
 using Microsoft.VisualStudio.Utilities;
 
@@ -12,11 +16,35 @@ namespace ProjectFileTools.Adornments
     [Name("MarkerFormatDefinition/HighlightWordFormatDefinition")]
     internal class HighlightWordFormatDefinition : MarkerFormatDefinition
     {
+        /// <summary>
+        /// Refers to MEF Items category (where the Highlighted Reference item is located)
+        /// </summary>
+        private readonly Guid _textEditorCategory = new Guid("{75A05685-00A8-4DED-BAE5-E7A50BFA929A}");
+
+        /// <summary>
+        /// Refers to the Highlighted Reference item
+        /// </summary>
+        private readonly string _itemName = "MarkerFormatDefinition/HighlightedReference";
+
         public HighlightWordFormatDefinition()
         {
-            this.BackgroundColor = Color.FromArgb(127, 170, 170, 170);
-            this.DisplayName = "Highlight Word";
-            this.ZOrder = 5;
+            // Default the BackgroundColor to a transparent grey
+            BackgroundColor = Color.FromArgb(127, 170, 170, 170);
+            DisplayName = "Highlight Word";
+            ZOrder = 5;
+
+            // If possible, set the Background color to match the Highlighted Reference color
+            IVsFontAndColorStorage colorStorage = ServiceProvider.GlobalProvider.GetService(typeof(IVsFontAndColorStorage)) as IVsFontAndColorStorage;
+            ColorableItemInfo[] itemInfoOut = new ColorableItemInfo[1];
+            if(colorStorage.OpenCategory(ref _textEditorCategory, (uint)(__FCSTORAGEFLAGS.FCSF_READONLY | __FCSTORAGEFLAGS.FCSF_LOADDEFAULTS) ) == VSConstants.S_OK)
+            {
+                if (colorStorage.GetItem(_itemName, itemInfoOut) == VSConstants.S_OK)
+                {
+                    uint hexColor = itemInfoOut[0].crBackground;
+                    BackgroundColor = Color.FromArgb(255, (byte)hexColor, (byte)(hexColor >> 8), (byte)(hexColor >> 16));
+                }
+                colorStorage.CloseCategory();
+            }
         }
     }
 }
