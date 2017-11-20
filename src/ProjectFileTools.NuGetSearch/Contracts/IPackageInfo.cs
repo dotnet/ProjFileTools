5dbab0a254f1e5ede05ea3090a9ac09895b7f543
Mikayla Hutchinson <m.j.hutchinson@gmail.com>
2017-11-20 01:47:43 -0500

    Fix incorrect argument order in local PackageInfo
    and add some more package properties

--- src/ProjectFileTools.NuGetSearch/Contracts/IPackageInfo.cs
+++ src/ProjectFileTools.NuGetSearch/Contracts/IPackageInfo.cs
@@ -6,17 +6,23 @@ namespace ProjectFileTools.NuGetSearch.Contracts
     {
         string Id { get; }
 
-        string IconUrl { get; }
+        string Version { get; }
 
-        string Description { get; }
+        string Title { get; }
 
         string Authors { get; }
+
+        string Summary { get; }
+
+        string Description { get; }
 
         string LicenseUrl { get; }
 
         string ProjectUrl { get; }
 
-        string Version { get; }
+        string IconUrl { get; }
+
+        string Tags { get; }
 
         FeedKind SourceKind { get; }
     }
--- src/ProjectFileTools.NuGetSearch/Feeds/NuSpecReader.cs
+++ src/ProjectFileTools.NuGetSearch/Feeds/NuSpecReader.cs
@@ -13,16 +13,19 @@ namespace ProjectFileTools.NuGetSearch.Feeds
             XElement package = doc.Root;
             XElement metadata = package?.Element(XName.Get("metadata", ns.NamespaceName));
             XElement id = metadata?.Element(XName.Get("id", ns.NamespaceName));
-            XElement title = metadata?.Element(XName.Get("title", ns.NamespaceName));
             XElement version = metadata?.Element(XName.Get("version", ns.NamespaceName));
+            XElement title = metadata?.Element(XName.Get("title", ns.NamespaceName));
             XElement authors = metadata?.Element(XName.Get("authors", ns.NamespaceName));
+            XElement summary = metadata?.Element (XName.Get ("summary", ns.NamespaceName));
             XElement description = metadata?.Element(XName.Get("description", ns.NamespaceName));
             XElement licenseUrl = metadata?.Element(XName.Get("licenseUrl", ns.NamespaceName));
-            XElement projectUrl = metadata?.Element(XName.Get("projectUrl", ns.NamespaceName));
+            XElement projectUrl = metadata?.Element (XName.Get ("projectUrl", ns.NamespaceName));
+            XElement iconUrl = metadata?.Element (XName.Get ("iconUrl", ns.NamespaceName));
+            XElement tags = metadata?.Element(XName.Get("tags", ns.NamespaceName));
 
             if (id != null)
             {
-                return new PackageInfo(id.Value, title?.Value ?? id.Value, version?.Value, authors?.Value, description?.Value, licenseUrl?.Value, projectUrl?.Value, kind);
+                return new PackageInfo(id.Value, version?.Value, title?.Value, authors?.Value, summary?.Value, description?.Value, licenseUrl?.Value, projectUrl?.Value, iconUrl?.Value, tags?.Value, kind);
             }
 
             return null;
