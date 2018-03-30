a03fdb99ede7018a95ad5237b1f7b60270693dfa
Mike Lorbetske <mlorbe@microsoft.com>
2018-03-29 22:41:38 -0700

    Adds QuickInfo for conditions

--- src/ProjectFileTools.MSBuild/IWorkspace.cs
+++ src/ProjectFileTools.MSBuild/IWorkspace.cs
@@ -9,5 +9,9 @@ namespace ProjectFileTools.MSBuild
         List<Definition> GetItemProvenance(string fileSpec);
 
         List<Definition> ResolveDefinition(string filePath, string sourceText, int position);
+
+        string GetEvaluatedPropertyValue(string text);
+
+        bool EvaluateCondition(string text);
     }
 }
