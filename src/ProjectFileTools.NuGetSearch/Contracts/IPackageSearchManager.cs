f07317973058069ae4a0a7b8abf298e6aed1d42f
Mikayla Hutchinson <m.j.hutchinson@gmail.com>
2020-12-11 21:52:31 -0500

    Add support for packageType query parameter

--- src/ProjectFileTools.NuGetSearch/Contracts/IPackageSearchManager.cs
+++ src/ProjectFileTools.NuGetSearch/Contracts/IPackageSearchManager.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using ProjectFileTools.NuGetSearch.Feeds;
 
 namespace ProjectFileTools.NuGetSearch.Contracts
@@ -6,9 +6,9 @@ namespace ProjectFileTools.NuGetSearch.Contracts
 
     public interface IPackageSearchManager
     {
-        IPackageFeedSearchJob<Tuple<string, FeedKind>> SearchPackageNames(string prefix, string tfm);
+        IPackageFeedSearchJob<Tuple<string, FeedKind>> SearchPackageNames(string prefix, string tfm, string packageType = null);
 
-        IPackageFeedSearchJob<Tuple<string, FeedKind>> SearchPackageVersions(string packageName, string tfm);
+        IPackageFeedSearchJob<Tuple<string, FeedKind>> SearchPackageVersions(string packageName, string tfm, string packageType = null);
 
         IPackageFeedSearchJob<IPackageInfo> SearchPackageInfo(string packageId, string version, string tfm);
     }
